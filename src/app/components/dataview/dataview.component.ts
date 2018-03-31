716d430333000e72251f3a020d3d05d26a03281f
Dominik Lasch <dl@exutec.de>
2018-03-31 14:42:42 +0200

    progress: current and prognosed data

--- src/app/components/dataview/dataview.component.ts
+++ src/app/components/dataview/dataview.component.ts
@@ -8,22 +8,57 @@ import { EnergydataService } from '../../services/energydata.service';
 })
 export class DataviewComponent implements OnInit {
 
-  energyData: Array<any>;
+  rawData: Array<any>;
+
+  currentEnergy: Array<any> = [];
+  prognosisEnergy: Array<any> = [];
+
+  getLastSunday(date) {
+    date.setDate(date.getDate() - date.getDay());
+    return date.setHours(24,0,0,0);
+  }
+  
+  // timestamp = this.getLastSunday(new Date());
+
+  powerGeneratorCategories = {
+    bio: 103, 
+    water: 1226, 
+    windOffshore: 1225, 
+    windOnshore: 100, 
+    pv: 102, 
+    otherRenewables: 1228, 
+    nuclear: 1224, 
+    brownCoal: 1223, 
+    hardCoal: 111, 
+    naturalGas: 112, 
+    pumpedStorage: 113, 
+    others: 1227,
+  };
+
+  powerPrognosis = {
+    all: 122,
+    windOffshore: 3791,
+    windOnshore: 123,
+    pv: 125,
+    others: 715
+  };
 
   constructor(public energyService: EnergydataService) { }
 
   ngOnInit() {
-    this.energyService.getAllData()
+    this.getCurrentProduction(this.powerGeneratorCategories, this.currentEnergy);
+    this.getCurrentProduction(this.powerPrognosis, this.prognosisEnergy);
+  }
+
+  async getCurrentProduction(object, array){
+    for(var key in object){
+      await this.energyService.getAllData(this.getLastSunday(new Date()), object[key])
       .then(data => {
-        console.log(data);
-        this.energyData = JSON.parse(data.contents).series;
-        this.energyService.energyData = JSON.parse(data.contents).series;
+        this.rawData = JSON.parse(data.contents).series;
+        let index = this.rawData.length;
+        while (index-- && !this.rawData[index][1]);
+        return array.push([key, this.rawData[index]]);
       })
-        // .then(res  => {
-        //   let index = res.findIndex((element) => {
-        //     return element.includes(null);
-        //   });
-        //   return res[index-1];
-        // })
+    }
   }
 }
--- src/app/services/energydata.service.ts
+++ src/app/services/energydata.service.ts
@@ -9,43 +9,9 @@ export class EnergydataService {
 
   energyData;
 
-  getLastSunday(date) {
-    date.setDate(date.getDate() - date.getDay());
-    return date.setHours(24,0,0,0);
-  }
-  
-  // timestamp = this.getLastSunday(new Date());
-
-  powerGeneratorCategories = {
-    bio: 103, 
-    water: 1226, 
-    windOffshore: 1225, 
-    windOnshore: 100, 
-    pv: 102, 
-    otherRenewables: 1228, 
-    nuclear: 1224, 
-    brownCoal: 1223, 
-    hardCoal: 111, 
-    naturalGas: 112, 
-    pumpedStorage: 113, 
-    others: 1227 
-  };
-
-  powerPrognosis = {
-    all: 122,
-    windOffshore: 3791,
-    windOnshore: 123,
-    pv: 125,
-    others: 715
-  };
-
-  
   constructor(public http: HttpClient) { }
   
-  getAllData() {
-    let energyKind = this.powerGeneratorCategories.pv;
-    let timestamp = this.getLastSunday(new Date());
-    console.log(timestamp);
+  getAllData(timestamp, energyKind) {
     let apiUrl = 'https://www.smard.de/app/chart_data/' + energyKind + '/DE/' + energyKind + '_DE_quarterhour_'+ timestamp +'.json';
     return $.getJSON('http://allorigins.me/get?url='+encodeURI(apiUrl)+'&callback=?');
   }
